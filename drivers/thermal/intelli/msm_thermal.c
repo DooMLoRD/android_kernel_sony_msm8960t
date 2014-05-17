4e4fb0198a1fab69d1a80218692a49a190cd8c9d
DooMLoRD <metoo.mememe@gmail.com>
2014-05-18 02:54:27 +0530

    intelli-thermal: increase sampling time to reduce sampling overhead
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/thermal/intelli/msm_thermal.c
+++ drivers/thermal/intelli/msm_thermal.c
@@ -27,7 +27,7 @@
 #include <linux/msm_thermal.h>
 #include <mach/cpufreq.h>
 
-#define DEFAULT_POLLING_MS	250
+#define DEFAULT_POLLING_MS	500
 /* last 3 minutes based on 250ms polling cycle */
 #define MAX_HISTORY_SZ		((3*60*1000) / DEFAULT_POLLING_MS)
 
